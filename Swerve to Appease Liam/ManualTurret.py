2c8d4bca39b3d94c58afe17c8c5cd0cc130d4998
Tim Henkins <ethanrgroves4@gmail.com>
2020-02-12 14:30:19 -0800

    Update ManualTurret.py

--- Swerve to Appease Liam/ManualTurret.py
+++ Swerve to Appease Liam/ManualTurret.py
@@ -2,11 +2,14 @@ import wpilib
 import rev
 
 class ManualTurret:
-	def __init__(self, rotateID, flyWheelID, speed):
+	def __init__(self, rotateID, flyWheelID, speed, threshold):
 		self.rotateMotor = rev.CANSparkMax(rotateID, MotorType.kBrushless)
 		self.rotateEncoder = self.rotateMotor.getEncoder()
+		self.encoderConversion = 20
+		self.rotateEncoder = setPositionConversionFactor(self.encoderConversion)
 		self.flyWheelMotor = rev.CANSparkMax(flyWheelID, MotorType.kBrushless)
 		self.speed = speed
+		self.threshold = threshold
 	def rotate(self):
-		if self.rotateEncoder < threshold:
+		if self.rotateEncoder < self.threshold:
 			pass
