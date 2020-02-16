19736bc578b41224a2ce36f6bdb655a9ff2e4e04
Scouting753 <team753code@gmail.com>
2020-02-16 15:38:12 -0800

    fixed turret stuff

--- Swerve to Appease Liam/ManualTurret.py
+++ Swerve to Appease Liam/ManualTurret.py
@@ -5,13 +5,24 @@ class ManualTurret:
 	def __init__(self, flyWheelID):
 		self.flyWheelMotor = rev.CANSparkMax(flyWheelID, MotorType.kBrushless)
 		self.flyWheelEncoder = self.flyWheelMotor.getEncoder()
+		self.flyWheelController = self.flyWheelMotor.getPIDController()
+		self.kP = 0.005
+		self.kI = 0
+		self.kD = 0
+		self.kIZone = 0
+		self.kFF = 1
+		self.flyWheelController.setP(self.kP)
+		self.flyWheelController.setI(self.kI)
+		self.flyWheelController.setD(self.kD)
+		self.flyWheelController.setIZone(self.kIZone)
+		self.flyWheelController.setFF(self.kFF)
 		
 	def spin(self, goalSpeed):
 		percent = goalSpeed/5700
 		self.flyWheelMotor.set(percent)
 		
 	def spinPID(self, goalSpeed):
-		
+		self.flyWheelController.setReference(goalSpeed, ControlType.kVelocity)
 		
 	def spinStop(self):
 		self.flyWheelMotor.set(0)
