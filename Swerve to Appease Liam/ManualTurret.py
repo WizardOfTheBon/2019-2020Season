955ccdfa3976930b0e731d2e342d138013722d85
Scouting753 <team753code@gmail.com>
2020-02-16 14:56:47 -0800

    started on pid for new manual turret

--- Swerve to Appease Liam/ManualTurret.py
+++ Swerve to Appease Liam/ManualTurret.py
@@ -4,10 +4,14 @@ import rev
 class ManualTurret:
 	def __init__(self, flyWheelID):
 		self.flyWheelMotor = rev.CANSparkMax(flyWheelID, MotorType.kBrushless)
+		self.flyWheelEncoder = self.flyWheelMotor.getEncoder()
 		
 	def spin(self, goalSpeed):
 		percent = goalSpeed/5700
 		self.flyWheelMotor.set(percent)
+		
+	def spinPID(self, goalSpeed):
+		
 		
 	def spinStop(self):
 		self.flyWheelMotor.set(0)
